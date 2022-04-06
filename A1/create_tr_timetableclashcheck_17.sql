f57a9bf69f3b721761a050ca095cb9549d1216eb
heathensiren <62172932+heathensiren@users.noreply.github.com>
2022-04-06 21:59:49 +1000

    Add files via upload

---
+++ A1/create_tr_timetableclashcheck_17.sql
@@ -0,0 +1,5 @@
+/* There should not be a timetable clash for a student or staff member
+If a student or staff member is assigned a timeslot that overlaps with an existing timetable slot then it should be rejected and a warning message generated
+Trigger checkTimetableClashStudent and checkTimeTableClashStaff 
+STEPS:
+1. */
